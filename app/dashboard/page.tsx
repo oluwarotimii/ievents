47dbb8a3691bff3e48a2091e0ca2a4f1636b0844
oluwarotimii <oluwarotimiadewumi@gmail.com>
2025-04-24 03:31:07 +0100

    fix:mobile UI and payment fix - loading context error

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -31,7 +31,7 @@ import { Badge } from "@/components/ui/badge"
 import { getUserForms, deleteForm } from "../actions/form-actions"
 import { logoutUser, getCurrentUserSubscriptionInfo } from "../actions/auth-actions"
 import { useRouter } from "next/navigation"
-import { useLoading } from "@/contexts/loading-context"
+import { useLoading } from "@/components/loading-context"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 
 interface FormData {
